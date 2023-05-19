ae214ae71566a0c0d1c8be13e09895b387ed5361
Kirandeep <kirandeep1195@gmail.com>
2023-05-19 10:16:17 -0400

    updated passLen and allchars variable

--- script.js
+++ script.js
@@ -4,12 +4,15 @@ var lowercase = "abcdefghijklmnopqrstuvwxyz"
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var number = "0123456789";
 var symbol = "!@#$%^&*+<></>";
-var allchars= "";
+//var allchars= "";
 function writePassword() {
+  var allchars = "";
   let password = "";
   var length = parseInt(prompt('Enter length of password'));
        if(length < 8 || length > 128){
-  var passLen = parseInt(prompt('Password must be between 8 to 128 characters'))};
+  var passLen = ('Password must be between 8 to 128 characters')
+return "alertpassLen"
+};
 
   var useLowercase= confirm('would you like to use lowercase');  
        if(useLowercase == true){
